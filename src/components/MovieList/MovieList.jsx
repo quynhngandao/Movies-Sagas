9977f90fb2becc4f24196a44374c4827d18e68e7
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-21 23:18:18 -0500

    fixed movielist

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import MovieItem from "./MovieItem";
+import "./MovieList.css";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
+  const history = useHistory();
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <section className="movies">
+        {movies.map((movie) => {
+          return (
+            <div key={movie.id} className="container">
+              <MovieItem movie={movie}/>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
+
