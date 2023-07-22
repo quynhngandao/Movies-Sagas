730e3750ea0d66a3cf84da09185d9014cb3bfde6
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-21 23:15:36 -0500

    added app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,35 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList'
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import MovieList from "../MovieList/MovieList";
+import MovieDetail from "../MovieDetail/MovieDetail";
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
+        {/* Nav bar */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Display list Pages */}
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
         {/* Details page */}
-
-        {/* Add Movie page */}
+        {/* Route parameters */}
+        <Route path="/details/:id" >
+          <MovieDetail />
+        </Route>
       </Router>
     </div>
   );
 }
 
-
 export default App;
+
